e71be8554d167a4322579202d5600e2bf0900b13
Lucia-Ay <0itslucia0@gmail.com>
2023-08-10 03:55:43 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: C program, prints with put function
+*
+* Return: 0 [success]
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
