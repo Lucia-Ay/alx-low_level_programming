8d2c591141ad15b2bf60d6d2a544619ca87e3b57
Lucia-Ay <0itslucia0@gmail.com>
2023-08-10 05:05:50 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program, prints a quote to the standard error
+ *
+ * Return: 1 [fail]
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 95);
+	return (1);
+}
