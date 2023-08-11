a0b746023f662fd93deef4e55aa13d4c8a6c2828
Lucia-Ay <0itslucia0@gmail.com>
2023-08-11 08:45:05 -0700

    0 to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Description: prints all possible combinations of two two-digit numbers
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i > 0 || j > 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
