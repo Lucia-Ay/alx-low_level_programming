08fd97b1c4aa16459cbad97392e97b6005e58b7a
Lucia-Ay <0itslucia0@gmail.com>
2023-08-15 08:18:33 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,14 @@
 /**
  * _islower - checks for lowercase characters
  *
+ * @c: argument of function
+ *
  * Return: 1 if 'c' is lowercase, otherwise 0
 */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
