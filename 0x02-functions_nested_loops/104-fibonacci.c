bc84c5a931c31cd360c6be0d7ad44e49afd6698a
Lucia-Ay <0itslucia0@gmail.com>
2023-08-15 12:10:34 -0700

    fibonacci suck

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,27 +33,28 @@ int main(void)
 {
 	int count, initial0s;
 	unsigned long fib1 = 1, fib2 = 2, sum, max = 100000000;
-	unsigned long fib1_pt2 = 0, fib2_pt2 = 0, sum_pt2 = 0;
+	unsigned long fib1pt2 = 0, fib2pt2 = 0, sumpt2 = 0;
 
 	for (count = 1; count < 99; count++)
 	{
-		if (fib1_pt2 > 0)
-			printf("%ld", fib1_pt2);
+		if (fib1pt2 > 0)
+			printf("%ld", fib1pt2);
 		initial0s = numLength(max) - 1 - numLength(fib1);
 
-		while (fib1_pt2 > 0 && initial0s > 0)
+		while (fib1pt2 > 0 && initial0s > 0)
 		{
 			printf("%d", 0);
 			initial0s--;
 		}
-		printf("%ld", fib1;
+
+		printf("%ld", fib1);
 
 		sum = (fib1 + fib2) % max;
-		sum_pt2 = fib1_pt2 + fib2_pt2 + (fib1 + fib2) / max;
+		sumpt2 = fib1pt2 + fib2pt2 + (fib1 + fib2) / max;
 		fib1 = fib2;
-		fib1_pt2 = fib2_pt2;
+		fib1pt2 = fib2pt2;
 		fib2 = sum;
-		fib2_pt2 = sum_pt2;
+		fib2pt2 = sumpt2;
 
 		if (count != 98)
 			printf(", ");
