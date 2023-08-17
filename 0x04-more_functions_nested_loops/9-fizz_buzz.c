321b16a7417dc3668f7ae7aabedebacd6d3e73cf
Lucia-Ay <0itslucia0@gmail.com>
2023-08-17 04:55:43 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100 & checks if they are divisable by 3 or 5 or both
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else if (num % 15 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+		if (num < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
