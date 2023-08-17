637fdf40bb75f783d44ad023a84402e57182f1e2
Lucia-Ay <0itslucia0@gmail.com>
2023-08-17 05:05:11 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints 1 to 100 & checks if they are divisable by 3 or 5 or both
@@ -12,12 +13,12 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
-			printf("Fizz");
-		else if (num % 5 == 0)
-			printf("Buzz");
-		else if (num % 15 == 0)
+		if (num % 15 == 0)
 			printf("FizzBuzz");
+		else if (num % 5 == 0 && !(num % 3 == 0)
+			printf("Buzz");
+		else if (num % 3 == 0 && !(num % 5 == 0)
+			printf("Fizz");
 		else
 			printf("%d", num);
 		if (num < 100)
